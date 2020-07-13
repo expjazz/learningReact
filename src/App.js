4057fdab7c36b1decdc1a28efe5daf1687925b56
Expedito Andrade <expeditojazz@gmail.com>
2020-07-13 13:51:14 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import Movies from './components/Movies';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Customers from './components/Customers';
 import Rentals from './components/Rentals';
 import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
+    <>
+    <NavBar />
     <div className='container'>
+      <Switch >
       <Route path="/movies" component={Movies}></Route>
       <Route path="/customers" component={Customers}></Route>
       <Route path="/rentals" component={Rentals}></Route>
       <Route path="not-found" component={NotFound}></Route>
+      <Redirect from="/" exact to="movies" />
+      <Redirect to="not-found" />
+      </Switch>
     </div>
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="#">Navbar</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <NavLink className="nav-link" to="/movies"> Movies <span className="sr-only">(current)</span></NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/customers">Customers</NavLink>
+      </li>
+
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
+      </li>
+    </ul>
+
+</nav>
+  )
+}
