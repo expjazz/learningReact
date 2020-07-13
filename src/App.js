250395f18addaadbdde87bc65ee110fc60db6c81
Expedito Andrade <expeditojazz@gmail.com>
2020-07-13 11:08:14 -0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Movies from './components/Movies';
+import { Route } from 'react-router-dom';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div>
-      <Movies />
+    <div className='container'>
+      <Route path="/movies" component={Movies}></Route>
+      <Route path="/customers" component={Customers}></Route>
+      <Route path="/rentals" component={Rentals}></Route>
+      <Route path="not-found" component={NotFound}></Route>
     </div>
   );
 }
