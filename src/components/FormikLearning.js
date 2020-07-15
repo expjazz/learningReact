8b97a1c8e48116a9535e200cf097bc7793bcf379
Expedito Andrade <expeditojazz@gmail.com>
2020-07-15 15:17:47 -0300

    formik naive implementation

--- src/components/FormikLearning.js
+++ src/components/FormikLearning.js
@@ -2,21 +2,49 @@ import React from "react";
 import { useFormik } from "formik";
 import { Input } from "./common/Input";
 export default function FormikLearning() {
+  const validate = values => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = 'Required';
+    } else if (values.name.length > 15) {
+      errors.name = 'Must be 15 characters or less';
+    }
+  
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length > 20) {
+      errors.password = 'Must be 20 characters or less';
+    }
+  
+    if (!values.username) {
+      errors.username = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.username)) {
+      errors.username = 'Invalid email address';
+    }
+  
+    return errors;
+  }
   const formik = useFormik({initialValues: {
     username: '',
     name: '',
     password: ''
-  }, onSubmit: values => {
-    console.log(formik)
+  }, validate, onSubmit: values => {
+    console.log(formik.values)
   }})
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="username">username</label>
       <input type="text" id="username" onChange={formik.handleChange} value={formik.values.username}/>
+      {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+
       <label htmlFor="password">password</label>
       <input type="password" id="password" onChange={formik.handleChange} value={formik.values.password}/>
+      {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+
       <label htmlFor="name">name</label>
       <input type="text" id="name" onChange={formik.handleChange} value={formik.values.name}/>
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+
       
       <button type='submit'>Submit</button>
     </form>
